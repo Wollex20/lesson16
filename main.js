50eaf309fa8d505dc9430b7fed9d28bca58ba269
Wollex20 <luarsabishvili.s20@gmail.com>
2024-11-28 20:52:50 +0400

    Done

---
+++ main.js
@@ -0,0 +1,39 @@
+const myArray = [
+  (firstProductPrice = 100),
+  (secondPoductPrice = 200),
+  (thirdProductPrice = 300),
+  (fourProductPrice = 400),
+  (fiveProductPrice = 500),
+];
+
+const sumPrices =
+  firstProductPrice +
+  secondPoductPrice +
+  thirdProductPrice +
+  fourProductPrice +
+  fiveProductPrice;
+textSumPrices = "რიცხვების ჯამი =";
+
+const Arithmetic = sumPrices / 5;
+textArithmatic = "საშვალო არითმეტიკული = ";
+
+console.log(myArray, textSumPrices, sumPrices, textArithmatic, Arithmetic);
+
+  const dateUse = {
+    title: ["A Tale of Two Cities"],
+    author: ["ჩარლზ დიკენსი"],
+    yearPublishedd: ["November 26, 1859"],
+  };
+  const Description =
+    "The novel tells the story of the French Doctor Manette, his 18-year-long imprisonment in the Bastille in Paris, and his release to live in London with his daughter Lucie whom he had never met.";
+  console.table(dateUse);
+  console.log(Description);
+  
+  // 1.17
+  
+  const propertyObjeq = [
+    { firstName: "name" },
+    { allAgeis: "age" },
+    { firstScore: "score" },
+  ];
+  console.log(propertyObjeq);
